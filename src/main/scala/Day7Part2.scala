3ccdced74dff29a508f4a811f9308db8980ab112
Santi <santi@odds.team>
2021-12-07 16:50:19 +0900

    add day7 part2

---
+++ src/main/scala/Day7Part2.scala
@@ -0,0 +1,35 @@
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day7Part2 extends App {
+  @tailrec
+  def leastFuelCost(
+      input: List[Int],
+      position: Int,
+      min: Int
+  ): Int = {
+    position match {
+      case 0 =>
+        min
+      case p =>
+        val sum = input
+          .map(in => {
+            (1 to (in - p).abs).sum
+          })
+          .sum
+        leastFuelCost(
+          input,
+          p - 1,
+          if (min > sum) sum else min
+        )
+    }
+  }
+  val source = Source.fromFile("src/input/day7.txt")
+
+  val sourceList: List[String] =
+    source.getLines().filterNot(_.isEmpty).toList
+  val sources = sourceList.head.split(",").map(_.toInt).toList
+
+  val result = leastFuelCost(sources, sources.max, Int.MaxValue)
+  println(result)
+}
